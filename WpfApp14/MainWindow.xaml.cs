8cd2aeee9ac8a828bf1cc994ac89a8b8a5f2ee23
SaMiAlex <samias@mail.ru>
2021-11-25 12:07:38 +0600

    Добавьте файлы проекта.

---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                Name = "Холодильник",
+                Price = 40000,
+                Location = "Data/1.jpg",
+                ProductType = ProductTypes.Electronic
+            });
+            products.Add(new Product()
+            {
+                Name = "Хлеб",
+                Price = 40,
+                Location = "Data/2.png",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Вода",
+                Price = 80,
+                Location = "Data/2.png",
+                ProductType = ProductTypes.Food
+            });
+
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ WpfApp14/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp14
+{
+    public enum ProductTypes
+    {
+        Electronic,
+        Food
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Price { get; set; }
+        public ProductTypes ProductType { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return ProblemName;
+        //}
+    }
+}
